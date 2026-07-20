f19d8e831ba88729b045a174816fa60d29f1ec64
nargeloooooooooooooooo <barmakeche@gmail.com>
2026-07-20 21:26:24 +0700

    Create init.py

---
+++ handlers/init.py
@@ -0,0 +1,6 @@
+from .admin import AdminHandlers
+from .user import UserHandlers
+from .group import GroupHandlers
+from .games import GameHandlers
+
+__all__ = ['AdminHandlers', 'UserHandlers', 'GroupHandlers', 'GameHandlers']
